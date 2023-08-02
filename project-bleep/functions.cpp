6bebf038fb5948eb024f873d34c5c5c41f0024c5
ARBUCHELI <radio_seattle@hotmail.com>
2023-08-02 11:57:14 +0300

    project bleep

---
+++ project-bleep/functions.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include "functions.hpp"
+
+void bleep(std::string word, std::string &sentence) {
+    
+  for (int i = 0; i < sentence.size(); i++) {
+    int match = 0;   
+
+    for (int j = 0; j < word.size(); j++) {
+      if (sentence[i+j] == word[j]) {
+        match++;   
+      }
+    }
+        
+    if (match == word.size()) {
+      for (int k = 0; k < word.size(); k++) {
+        sentence[i+k] = '*';
+      }
+    }  
+  }   
+}

3e02256f07f9c0c3c9c397c8a94b76f5988b3fe1
ARBUCHELI <radio_seattle@hotmail.com>
2023-08-02 10:55:49 +0300

    review

---
+++ letter.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  std::cout << "I'll be the best Software Developer that I can!\n";
+  std::cout << "Andres R. Bucheli - 28/07/2023";
+}
